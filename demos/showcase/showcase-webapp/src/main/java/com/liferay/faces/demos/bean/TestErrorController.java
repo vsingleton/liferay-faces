541d1b9aa785a93a3cc37b03f8db4e9d9596f067
vsingleton <vsingleton@gmail.com>
2015-04-28 09:52:29 -0400

    liferay-faces-prime5: Oleg Efimov - PrimeFaces ajaxExceptionHandler fails under Liferay Faces Bridge - https://www.liferay.com/community/forums/-/message_boards/view_message/53059360 - initial commit

---
+++ demos/showcase/showcase-webapp/src/main/java/com/liferay/faces/demos/bean/TestErrorController.java
@@ -0,0 +1,30 @@
+package com.liferay.faces.demos.bean;
+
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "testError")
+@SessionScoped
+public class TestErrorController {
+    protected boolean throwRenderException;
+
+    public void causeActionError() {
+        throw new IllegalArgumentException("causeActionError: Deliberate action exception");
+    }
+
+    public void causeRenderError() {
+    	System.err.println("causeRenderError: ...");
+        throwRenderException = true;
+    }
+
+    public String getStatus() {
+        if (throwRenderException) {
+            throwRenderException = false;
+            throw new IllegalArgumentException("getStatus: Deliberate render exception");
+        } else {
+            return "getStatus: Updated at " + new Date();
+        }
+    }
+}
